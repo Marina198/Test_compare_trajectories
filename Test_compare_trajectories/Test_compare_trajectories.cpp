fc3903ad1a350f73efeb613abd0db1eb658dd97d
Marina Ldova <marina.ldova@gmail.com>
2023-05-17 05:23:22 -0700

    new

--- Test_compare_trajectories/Test_compare_trajectories.cpp
+++ Test_compare_trajectories/Test_compare_trajectories.cpp
@@ -1,135 +1,210 @@
-﻿// Test_compare_trajectories.cpp 
-//
-
-#include <iostream>
-#include <fstream>
-#include <vector>
+﻿//
+﻿#include <iostream>
 #include <map>
-#include <math.h>
+#include <string_view>
+#include <functional>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+#include <algorithm>
+#include <iterator>
+#include <unordered_map>
+#include <utility>
+#include <cmath>
+#include <numeric>
+#include <stdexcept>
+#include <limits>
+#include <variant>
 
-#include "CTrajectory.h"
-
-
-// FUNCTION:	readTrajFromFile 
-// DESCRIPTION: function of reading trajectories data from file 
-//              and calculating different metrics connecting with trajectories
-// INPUT :		filename - file name 
-// OUTPUT:		vector<CTrajectory> - vector of readed trajectories
-// 
-std::vector<CTrajectory>  readTrajFromFile(const std::string& filename)
+struct Trajectory
 {
-    std::ifstream iFile_Trajectories(filename, std::ios::binary);
-
-    // checking if we can't open file for reading its content
-    if (!iFile_Trajectories.is_open())
+    struct Point
     {
-        throw std::runtime_error("Failed to open file " + filename);       
+        int x;
+        int y;
+    };
+
+    using TimeMarker = int;
+    using TimePoint = std::pair<TimeMarker, Point>;
+    using Time2PointMap = std::multimap<TimeMarker, Point>;
+
+    friend std::istream& operator>>(std::istream& is, TimePoint& time_point) {
+        auto& [time_marker, point] = time_point;
+        is >> point.x >> point.y;
+        is >> time_marker;
+        return is;
     }
 
-    uint32_t iNumOfTrajectories = 0;
-    std::vector<CTrajectory> vTestTrajectories;
-    
-    if (!iFile_Trajectories.eof())
-    {
-        iFile_Trajectories >> iNumOfTrajectories;
-        std::cout << "Number of trajectories: " << iNumOfTrajectories << std::endl;
+    friend std::istream& operator>>(std::istream& is, Trajectory& traj) {
+        int num_points;
+        is >> num_points;
 
-        uint32_t iNumTraj = 0;   
-       
-        while (iNumOfTrajectories--)
-        {
-            uint32_t iNumOfPoints = 0;
+        std::copy_n(std::istream_iterator<TimePoint>(is), num_points, std::inserter(traj.points_, traj.points_.end()));
 
-            iFile_Trajectories >> iNumOfPoints;
-            std::cout << "In " << ++iNumTraj << " trajectory " << iNumOfPoints << " points:" << std::endl;
+        return is;
+    }
 
-            CTrajectory testTraj;
+    friend std::ostream& operator<<(std::ostream& os, const Trajectory& traj) {
+        for (const auto [time_marker, point] : traj.points_) {
+            os << point.x << ' ' << point.y << ' ' << time_marker << ' ';
+        }
+        return os;
+    }
 
-            while (iNumOfPoints--)
-            {
-                int32_t x, y, t;
-                iFile_Trajectories >> x >> y >> t;
-                testTraj.addPoint(t, x, y);
-            }
-            testTraj.printMap();
+    Time2PointMap points_;
+};
 
-            // Calculate the Length of tested trajectories
-            testTraj.calculateLength();
+class App {
+public:
+    using MetricNameToFunctorMap = std::map<std::string_view, std::function<double(const Trajectory&)>>;
+    App(int argc, char* argv[], MetricNameToFunctorMap metrics, std::istream& is = std::cin, std::ostream& os = std::cout)
+        : is_{ is }
+        , os_{ os }
+        , metrics_{ std::move(metrics) } {
+        if (argc < 3)
+            throw std::runtime_error("Usage: Test_compare_trajectories <file_name> <metric1> <metric2> ... <metricN>");
 
-            // Calculate the average speed of tested trajectories
-            testTraj.calculateAverageSpeed(testTraj.getPoints().rbegin()->first - testTraj.getPoints().begin()->first);
+        if (!std::filesystem::exists(argv[1]))
+            throw std::runtime_error("File not found: " + std::string(argv[1]));
 
-            vTestTrajectories.push_back(testTraj);
+        file_name_ = argv[1];
+
+        auto check_metric = [this](char* metric) {
+            auto check_metric = [this](char* metric) {
+                try {
+                    applied_metrics_.emplace_back(metrics_.at(metric));
+                }
+                catch (const std::out_of_range&) {
+                    throw std::runtime_error("Unknown metric: " + std::string(metric));
+                }
+            };
+        };
+
+        // std::span<char*> vMetrics(argv + 2, argv + argc); // C++20
+        std::vector<char*> vMetrics(argv + 2, argv + argc);
+
+        for (auto& metric : vMetrics) {
+            check_metric(metric);
         }
     }
 
-    // close file with test trajectories 
-    iFile_Trajectories.close();
-    std::cout << std::endl << std::endl;
+    virtual ~App() = default;
 
-    return vTestTrajectories;
-}
+    int Run() {
+        std::ifstream file(file_name_);
 
+        int num_trajectories;
+        file >> num_trajectories;
 
-// Main function of the programm
-//
-int main()
+        while (num_trajectories--)
+        {
+            Trajectory traj;
+            file >> traj;
+            trajectories_.emplace_back(traj);
+        }
+
+        is_ >> reference_trajectory_;
+
+        for (auto& metric : applied_metrics_) {
+            ApplyMetric(metric);
+        }
+
+        return 0;
+    }
+protected:
+    using MetricResults = std::vector<std::pair<double, size_t>>;
+    virtual void Result(const std::string_view metric_name, double reference_result, MetricResults trajectories_results) {
+        std::sort(trajectories_results.begin(), trajectories_results.end(),
+            [reference_result](const auto& lhs, const auto& rhs) {
+                return std::fabs(lhs.first - reference_result) < std::fabs(rhs.first - reference_result);
+            });
+
+        int num_closest = trajectories_results.size() < 3 ? trajectories_results.size() : 3;
+        os_ << std::fixed << std::setprecision(2);
+        os_ << num_closest << " closest trajectories by " << metric_name << ": " << reference_result << ",  to reference trajectory:\n";
+        std::for_each_n(trajectories_results.begin(), num_closest, [this, &metric_name, reference_result](const auto& traj) {
+            auto [metric, index] = traj;
+            os_ << "Trajectory: " << index << ", " << metric_name << ": " << metric << ", diff: " << std::fabs(traj.first - reference_result) << '\n';
+            });
+    }
+
+private:
+    template<typename MetricFunctor>
+    MetricResults CalculateTrajectoriesResults(MetricFunctor& metric_functor) {
+        MetricResults trajectories_results;
+        for (size_t i = 0; i != trajectories_.size(); ++i) {
+            trajectories_results.emplace_back(metric_functor(trajectories_[i]), i);
+        }
+        return trajectories_results;
+    }
+
+    void ApplyMetric(MetricNameToFunctorMap::value_type& metric) {
+        auto& [metric_name, metric_functor] = metric;
+        MetricResults trajectories_results = CalculateTrajectoriesResults(metric_functor);
+        auto reference_result = metric_functor(reference_trajectory_);
+        Result(metric_name, reference_result, std::move(trajectories_results));
+    }
+
+private:
+    std::istream& is_;
+    std::ostream& os_;
+    MetricNameToFunctorMap metrics_;
+    std::string file_name_;
+    std::vector<MetricNameToFunctorMap::value_type> applied_metrics_;
+    std::vector<Trajectory> trajectories_;
+    Trajectory reference_trajectory_;
+    // todo: add cache of calculated metrics
+};
+
+//todo: fabric method for metrics
+auto lengthMetric = [](const Trajectory& traj) -> double {
+    const auto& points = traj.points_;
+
+    auto it = points.begin();
+    auto itprev = it;
+    std::advance(it, 1);
+    double length = 0.0;
+
+    std::for_each(it, points.end(),
+        [&itprev, &length](const auto& current_point_pair) {
+            auto prev_point_pair = *itprev;
+            const auto& current_point = current_point_pair.second;
+            const auto& prev_point = prev_point_pair.second;
+
+            length += sqrt(pow(current_point.x - prev_point.x, 2) + pow(current_point.y - prev_point.y, 2));
+            ++itprev;
+        });
+    return length;
+};
+
+auto speedMetric = [](const Trajectory& traj) -> double {
+    auto points = traj.points_;
+    double result = 0.0;
+
+    if (auto total_time = points.rbegin()->first - points.begin()->first; total_time != 0) {
+        result = lengthMetric(traj) / total_time;
+    }
+
+    return result;
+};
+
+auto timeMetric = [](const Trajectory& traj) -> double {
+    return static_cast<double>(traj.points_.rbegin()->first - traj.points_.begin()->first);
+};
+
+int main(int argc, char* argv[])
 {
+    App::MetricNameToFunctorMap metrics{
+        {"length", lengthMetric},
+        {"speed", speedMetric},
+        {"time", timeMetric}
+    };
+
     try {
-        std::cout << "The program reads the reference trajectory from the given file:\n\n";
-        auto refTrajectory = readTrajFromFile("d:\\reference_trajectory.dat");
-
-        std::cout << "Now the program reads test trajectories from the given file:\n\n";
-        auto vTestTrajectories = readTrajFromFile("d:\\small_trajectory.dat");
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-        // put the metrics in the appropriate containers sorted in ascending order of the key
-        //
-        std::multimap<double, int> mLengthDifference;
-        std::multimap<double, int> mSpeedDifference;
-
-        for (size_t i = 0; i < vTestTrajectories.size(); ++i)
-        {
-            mLengthDifference.emplace(std::make_pair(abs(refTrajectory[0].getLength() - vTestTrajectories[i].getLength()), i));
-            mSpeedDifference.emplace(std::make_pair(abs(refTrajectory[0].getAverageSpeed() - vTestTrajectories[i].getAverageSpeed()), i));
-        }
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-
-        std::cout << "Choose the metric for comparison data from files, please: 1 - for length / 2 - for speed\n";
-        uint16_t iMetric = 0;
-        std::cin >> iMetric;
-
-        uint16_t iCount = 3;   // - number of displaying trajectories nearest to reference traj according chosen metrics 
-
-        if (iMetric == 1) // work with trajectory length
-        {
-            std::cout << "Reference traj Length = " << refTrajectory[0].getLength() << std::endl << std::endl;
-
-            for (auto it_Len_Dif = mLengthDifference.begin(); it_Len_Dif != mLengthDifference.end() && iCount--; ++it_Len_Dif)
-            {
-                std::cout << "Length dif of traj # " << it_Len_Dif->second + 1 << " = " << it_Len_Dif->first << std::endl;
-                vTestTrajectories[it_Len_Dif->second].printMap();
-                std::cout << std::endl;
-            }
-        }
-        else  // work with trajectory speed
-        {
-            std::cout << "Reference traj Avg Speed = " << refTrajectory[0].getAverageSpeed() << std::endl << std::endl;
-
-            for (auto it_Speed_Dif = mSpeedDifference.begin(); it_Speed_Dif != mSpeedDifference.end() && iCount--; ++it_Speed_Dif)
-            {
-                std::cout << "Speed dif of traj # " << it_Speed_Dif->second + 1 << " = " << it_Speed_Dif->first << std::endl;
-                vTestTrajectories[it_Speed_Dif->second].printMap();
-                std::cout << std::endl;
-            }
-        }
+        return App(argc, argv, metrics).Run();
     }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
+    catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return 1;
     }
-
-    return 0;   
 }
-
-
